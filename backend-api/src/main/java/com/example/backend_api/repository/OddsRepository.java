ef094c76498325e59ef5bcbe4a98ae4e18318148
qijiepeng92 <qijiepeng92@gmail.com>
2025-06-01 22:13:42 +0800

    add time based columns now requires scheduler

--- backend-api/src/main/java/com/example/backend_api/repository/OddsRepository.java
+++ backend-api/src/main/java/com/example/backend_api/repository/OddsRepository.java
@@ -6,5 +6,7 @@ import com.example.backend_api.model.Odds;
 import com.example.backend_api.model.OddsId;
 
 public interface OddsRepository extends JpaRepository<Odds, OddsId> {
-    // Integer is the type of primary key (matchid)
+   // OddsId is the composite key class (matchId + betOperatorId)
+   List<Odds> findByMatchId(Integer matchId); //Added by Qijie
+   Optional<Odds> findByMatchIdAndBetOperatorId(Integer matchId, Integer betOperatorId); //Added by Qijie
 }
